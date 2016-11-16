3cdb0eee849baccbdd9ee76d9e001ebb851e6517
周鹏钜 <1321932448@qq.com>
2016-11-16 12:38:56 +0800

    提交

--- drawDemo/drawDemo/Controller/implicitAnimationController.m
+++ drawDemo/drawDemo/Controller/implicitAnimationController.m
@@ -422,4 +422,45 @@
 //    self.layer.zPosition = 100.0;
 //}
 
+// 把文字和图片绘制到一起
+- (void)picture {
+
+    //    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    //    [self.view addSubview:imageView];
+    //    imageView.backgroundColor = [UIColor whiteColor];
+    //
+    //    // 创建一个bitmap的上下文
+    //    UIGraphicsBeginImageContextWithOptions(CGSizeMake(50, 50), YES, 0);
+    //
+    //    // 获取bitmap上下文
+    //    CGContextRef context = UIGraphicsGetCurrentContext();
+    //
+    //    // 绘图
+    ////    CGContextAddEllipseInRect(context, CGRectMake(0, 0, 50, 50));
+    ////
+    ////    // 渲染
+    ////    CGContextStrokePath(context);
+    //
+    //    CGContextFillRect(context, CGRectMake(0, 0, 50, 50));
+    //
+    //    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    //    UIGraphicsEndImageContext();
+    //    imageView.image = image;
+    //
+    //    NSData *data = UIImageJPEGRepresentation(image, 1);
+    //    [data writeToFile:@"" atomically:YES];
+    
+    //    UIImage *image = [UIImage imageNamed:@"2.jpg"];
+    //    UIGraphicsBeginImageContextWithOptions(image.size, YES, 0);
+    //    [image drawAtPoint:CGPointMake(0, 0)];
+    //    UIImage *image1 = [UIImage imageNamed:@"vip_ic_reason_1"];
+    //    [image1 drawAtPoint:CGPointMake(200, 200)];
+    //    NSString *str = @"我是超人";
+    //    [str drawAtPoint:CGPointMake(120, 120) withAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14]}];
+    //    UIImage *image11 = UIGraphicsGetImageFromCurrentImageContext();
+    //        UIGraphicsEndImageContext();
+    //    NSData *data = UIImageJPEGRepresentation(image11, 1);
+    //    [data writeToFile:@"/Users/zhoupengju/Desktop/test@.png" atomically:YES];
+}
+
 @end
